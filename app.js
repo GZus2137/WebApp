577e8faa077598061334d7aebde56ab3095a385b
GZus2137 <pieczarson17@gmail.com>
2024-01-17 20:16:54 +0100

    comit 5

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const express = require("express")
+// const express = require("express")
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.get('/', (req, res) =>{
+//     res.send('Hello, world!');
+// });
+
+// app.listen(port, () => {
+//     console.log(`Aplikacja dziala na porcie ${port}`);
+// });
+
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname)));
+
 app.get('/', (req, res) =>{
-    res.send('Hello, world!');
+    
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`Aplikacja dziala na porcie ${port}`);
+    console.log(`Aplikacja działa na porcie ${port}`);
 });
